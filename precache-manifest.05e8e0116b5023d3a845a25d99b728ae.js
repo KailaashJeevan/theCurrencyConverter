534d979164540dcce853a5f8fea323cd5651610e
kailaash <kailaash80@gmail.com>
2020-05-06 22:36:52 +0530

    Updates

---
+++ precache-manifest.05e8e0116b5023d3a845a25d99b728ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd2dc4ae1a7a5b674400744fc6bbe3d",
+    "url": "/thecurrencyconverter/index.html"
+  },
+  {
+    "revision": "be39622112cc8cf2efde",
+    "url": "/thecurrencyconverter/static/css/main.a72b578e.chunk.css"
+  },
+  {
+    "revision": "9d5b869a356d440a488b",
+    "url": "/thecurrencyconverter/static/js/2.852effd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/thecurrencyconverter/static/js/2.852effd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be39622112cc8cf2efde",
+    "url": "/thecurrencyconverter/static/js/main.6c5d2b67.chunk.js"
+  },
+  {
+    "revision": "87a96d32a7be6931a851",
+    "url": "/thecurrencyconverter/static/js/runtime-main.5fab25b6.js"
+  }
+]);
